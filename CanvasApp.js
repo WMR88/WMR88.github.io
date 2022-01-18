475080861a023c9118a09d524f1f15af1616aafc
William R <williamr882021@gmail.com>
2022-01-18 16:05:17 -0600

    image fix

--- CanvasApp.js
+++ CanvasApp.js
@@ -122,27 +122,5 @@ init();
 animate();
 
 
-// line 48
-// Collision Detection.
-// let dx = mouse.x - this.x;
-// let dy = mouse.y - this.y;
-// let distance = Math.sqrt(dx*dx + dy*dy);
-
-// if (distance < mouse.radius + this.size) {
-//     if(mouse.x < this.x && this.x < canvas.width - this.size * 10) {
-//         this.x += 10;
-//     }
-//     if(mouse.x > this.x && this.x > this.size * 10) {
-//         this.x -= 10;
-//     }
-//     if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-//         this.y += 10;
-//     }
-//     if (mouse.y > this.y && this.y > this.size * 10) {
-//         this.y -= 10;
-//     }
-// }
-
-
 
 
